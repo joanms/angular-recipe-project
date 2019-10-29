dc5cd8b71b86788416421ec9ef83237154c2257d
Joan Sweeney <joanms@eircom.net>
2019-10-29 16:02:38 +0000

    Add ingredients to the recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected =  new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is just a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-        new Recipe('Another Test Recipe', 'This is just a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+        new Recipe(
+            'Boxty',
+            'This is just a test',
+            'https://upload.wikimedia.org/wikipedia/commons/2/2a/Boxty_with_beef_and_squash.jpg',
+            [
+                new Ingredient('Potatoes', 6),
+                new Ingredient('Onions', 2)
+            ]),
+        new Recipe(
+            'Irish Stew',
+            'This is just a test',
+            'https://upload.wikimedia.org/wikipedia/commons/4/42/Irish_stew_2007.jpg',
+            [
+                new Ingredient('Lamb', 1),
+                new Ingredient('Carrots', 6)
+            ])
       ];
 
       getRecipes() {
