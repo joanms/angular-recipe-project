ef50c7cee657d213d70262698c5e640ca666f528
Joan Sweeney <joanms@eircom.net>
2019-11-20 15:28:51 +0000

    Enable storage of recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+
 // {providedIn: 'root'} is an alternative to adding it to providers in the app module
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http
+        .put(
+            'https://ng-recipe-book-880f8.firebaseio.com/recipes.json',
+            recipes
+        )
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
 }
