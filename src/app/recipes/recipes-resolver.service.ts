67c17b78e998123891e25a2c5454085b51f7b3d4
Joan Sweeney <joanms@eircom.net>
2019-11-20 16:43:26 +0000

    Prevent user changes from being overwritten by the database

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -7,12 +7,19 @@ import {
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
 
         resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-            return this.dataStorageService.fetchRecipes();
+            const recipes = this.recipeService.getRecipes();
+            // The following code prevents user changes from being overwritten by the database
+            if (recipes.length === 0) {
+                return this.dataStorageService.fetchRecipes();
+            } else {
+                return recipes;
+            }
         }
     }
