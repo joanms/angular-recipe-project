f45424c2f13368877cf471581f5d707e322804c4
Joan Sweeney <joanms@eircom.net>
2019-12-02 14:00:27 +0000

    Add authentication token to outgoing requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/Operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/Operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 // {providedIn: 'root'} is an alternative to adding it to providers in the app module
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -23,22 +24,26 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-        .get<Recipe[]>(
-            'https://ng-recipe-book-880f8.firebaseio.com/recipes.json')
-            /*
-            The following code handles cases where a user inputs a recipe without ingredients.
-            The instance of map on the next line of code is an rxjs operator
-            and the one on the line after that is a JavaScript array method
-            */
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-                });
-            }),
-            tap(recipes => {
-                this.recipeService.setRecipes(recipes);
-            })
-        );
+       return this.authService.user.pipe(
+           take(1),
+           exhaustMap(user => {
+            return this.http.get<Recipe[]>(
+                'https://ng-recipe-book-880f8.firebaseio.com//recipes.json',
+                {
+                    params: new HttpParams().set('auth', user.token)
+                }
+            );
+        }),
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        }));
+      }
     }
-}
