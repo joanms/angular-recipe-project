3603ef353ba52e90974924e727887fcfc3610660
Joan Sweeney <joanms@eircom.net>
2020-01-23 16:34:58 +0000

    Add entry component to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,6 +58,9 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
       useClass: AuthInterceptorService,
       multi: true
     }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule { }
