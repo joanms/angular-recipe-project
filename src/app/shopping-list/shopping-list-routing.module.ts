ce83e6b7072a330f1e06af645d6b47f232d878cc
Joan Sweeney <joanms@eircom.net>
2020-02-03 14:43:05 +0000

    Add shopping list module

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from '../auth/auth.component';
+import { ShoppingListComponent } from './shopping-list.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class ShoppingListRoutingModule {}
