47042f30b3c99fc7af570bb1745aa5d307795166
Joan Sweeney <joanms@eircom.net>
2020-03-22 18:03:27 +0000

    Enhance constants in auth and shopping list actions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import * as fromApp from '../store/app.reducer';
+
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -14,11 +18,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
     constructor(
         private dataStorageService: DataStorageService,
-        private authService: AuthService
+        private authService: AuthService,
+        private store: Store<fromApp.AppState>
     ) {}
 
     ngOnInit() {
-        this.userSub = this.authService.user.subscribe(user => {
+        this.userSub = this.store
+        .select('auth')
+        .pipe(map(authState => authState.user))
+        .subscribe(user => {
             // !!user ensures that it will be true if there is a user and false if there isn't
             this.isAuthenticated = !!user;
             console.log(!user);
