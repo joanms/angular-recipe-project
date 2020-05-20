bbafd5e00c5a79f2f3a8fd5465014a2f781e7794
Joan Sweeney <joanms@eircom.net>
2020-05-20 18:29:38 +0100

    Add auto-logout with NgRx

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -8,6 +8,7 @@ import { of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../user.model';
+import { AuthService } from '../auth.service';
 
 export interface AuthResponseData {
     kind: string;
@@ -72,7 +73,11 @@ export interface AuthResponseData {
             password: signupAction.payload.password,
             returnSecureToken: true
           }
-        ).pipe(
+        )
+        .pipe(
+          tap(resData => {
+            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+          }),
           map(resData => {
             return handleAuthentication(
               +resData.expiresIn,
@@ -101,6 +106,9 @@ export interface AuthResponseData {
                 returnSecureToken: true
               }
             ).pipe(
+              tap(resData => {
+                this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+              }),
                 map(resData => {
                   return handleAuthentication(
                     +resData.expiresIn,
@@ -118,7 +126,7 @@ export interface AuthResponseData {
 
     @Effect({dispatch: false})
     authRedirect = this.actions$.pipe(
-        ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT),
+        ofType(AuthActions.AUTHENTICATE_SUCCESS),
         tap(() => {
             this.router.navigate(['/']);
         })
@@ -147,6 +155,10 @@ export interface AuthResponseData {
 
           if (loadedUser.token) {
             // this.user.next(loadedUser);
+            const expirationDuration =
+              new Date(userData._tokenExpirationDate).getTime() -
+              new Date().getTime();
+            this.authService.setLogoutTimer(expirationDuration);
             return new AuthActions.AuthenticateSuccess({
                 email: loadedUser.email,
                 userId: loadedUser.id,
@@ -165,8 +177,15 @@ export interface AuthResponseData {
     @Effect({dispatch: false})
     authLogout = this.actions$.pipe(ofType(AuthActions.LOGOUT),
     tap(() => {
+      this.authService.clearLogoutTimer();
       localStorage.removeItem('userData');
+      this.router.navigate(['/auth']);
     }))
 
-    constructor(private actions$: Actions, private http: HttpClient, private router: Router) {}
+    constructor(
+      private actions$: Actions,
+      private http: HttpClient,
+      private router: Router,
+      private authService: AuthService
+    ) {}
 }
