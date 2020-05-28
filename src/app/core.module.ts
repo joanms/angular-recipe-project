cd21540f6bb9a00000483f92e632b56222e3e055
Joan Sweeney <joanms@eircom.net>
2020-05-28 17:22:09 +0100

    Remove unnecessary recipe service code

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RecipeService } from './recipes/recipe.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { LoggingService } from './logging.service';
 
 @NgModule({
     providers: [
-        RecipeService,
         {
           provide: HTTP_INTERCEPTORS,
           useClass: AuthInterceptorService,
           multi: true
         },
-        LoggingService
     ]
 })
 export class CoreModule {}
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -4,7 +4,6 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/Operators';
 
-import { RecipeService } from '../recipe.service';
 import * as fromApp from '../../store/app.reducer';
 import * as RecipesActions from '../store/recipe.actions';
 import { Subscription } from 'rxjs';
@@ -22,7 +21,6 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   private storeSub: Subscription;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService,
               private router: Router,
               private store: Store<fromApp.AppState>) {
     }
